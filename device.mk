7d921c3c85a6ecbfda1da9c45d31d26236a55d7c
Abhishek Aggarwal <warabhishek@gmail.com>
2019-08-08 13:20:45 +0000

    X00TD: Add DT2W support
    * Add the gestures supported by our stock rom
    * Rename some strings to match with stock
    
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- device.mk
+++ device.mk
@@ -351,7 +351,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
