9cbe4c460072d3bd13f3a1b60849a89d7c1f0f7c
Amit Somani <amitsm@codeaurora.org>
2019-12-19 15:54:15 +0530

    X00TD: camera: QCamera2: HAL3: Add new operation mode to disable post processing
    Change-Id: Iddc08688b029a5e7a98c9b1f98fdf052552d38ae

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -2910,19 +2910,24 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
                     mStreamConfigInfo[index].type[stream_index] = CAM_STREAM_TYPE_CALLBACK;
                 } else {
                     mStreamConfigInfo[index].type[stream_index] = CAM_STREAM_TYPE_CALLBACK;
-                    if (isOnEncoder(maxViewfinderSize, newStream->width, newStream->height)) {
-                        if (bUseCommonFeatureMask && !is_qcfa_stream)
-                            mStreamConfigInfo[index].postprocess_mask[stream_index] =
-                                    commonFeatureMask;
-                        else
-                            mStreamConfigInfo[index].postprocess_mask[stream_index] =
-                                    CAM_QCOM_FEATURE_NONE;
-                    }else {
+                    if(mOpMode == QCAMERA3_VENDOR_STREAM_CONFIGURATION_PP_DISABLED_MODE) {
                         mStreamConfigInfo[index].postprocess_mask[stream_index] =
-                                CAM_QCOM_FEATURE_PP_SUPERSET_HAL3;
-                    }
-                    if ((isZsl) && (zslStream == newStream)) {
-                        zsl_ppmask = mStreamConfigInfo[index].postprocess_mask[stream_index];
+                                    CAM_QCOM_FEATURE_NONE;
+                    } else {
+                        if (isOnEncoder(maxViewfinderSize, newStream->width, newStream->height)) {
+                            if (bUseCommonFeatureMask && !is_qcfa_stream)
+                                mStreamConfigInfo[index].postprocess_mask[stream_index] =
+                                        commonFeatureMask;
+                            else
+                                mStreamConfigInfo[index].postprocess_mask[stream_index] =
+                                        CAM_QCOM_FEATURE_NONE;
+                        }else {
+                            mStreamConfigInfo[index].postprocess_mask[stream_index] =
+                                    CAM_QCOM_FEATURE_PP_SUPERSET_HAL3;
+                        }
+                        if ((isZsl) && (zslStream == newStream)) {
+                            zsl_ppmask = mStreamConfigInfo[index].postprocess_mask[stream_index];
+                        }
                     }
                 }
             break;
@@ -3459,7 +3464,8 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
     // Only create analysis and callback streams if either the disable flag has
     // been set or if only RAW streams are present.
     bool createAnalysisAndCallbackStreams = true;
-    if (onlyRaw || disableSupportStreams || isDepth) {
+    if (onlyRaw || disableSupportStreams || isDepth ||
+        (mOpMode == QCAMERA3_VENDOR_STREAM_CONFIGURATION_PP_DISABLED_MODE)) {
         createAnalysisAndCallbackStreams = false;
     }
     if (createAnalysisAndCallbackStreams && (mCommon.needAnalysisStream() || isDualCamera())) {
