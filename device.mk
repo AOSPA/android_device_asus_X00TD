c44b89e684bb4783f2a1783d58b67b8327489899
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 23:43:08 +0530

    X00TD: Build external audio hal modules
    Change-Id: Ib00ae87142a533285ccfcf5d36876c7a93dd7082
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- device.mk
+++ device.mk
@@ -97,6 +97,13 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix
 
+PRODUCT_PACKAGES += \
+    libsndmonitor \
+    libhdmiedid \
+    liba2dpoffload \
+    libhfp
+
+# Audio Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
