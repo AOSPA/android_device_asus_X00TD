1296cddbf9c5f6e51b14074f6a7408af436129cc
SayanAcharya <rowdysayan99@gmail.com>
2020-03-24 08:27:32 +0000

    X00TD: Remove duplicate prop
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -110,7 +110,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
-    debug.sf.early_phase_offset_ns=5000000 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
