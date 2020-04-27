cd8df3bfcfc48ca1891c066f177f2357d3be0e0c
SayanAcharya <rowdysayan99@gmail.com>
2020-04-27 19:41:21 +0000

    X00TD: Add remaining bits in audio_policy_configuration
    *From stock 10 beta
    *Remove antradio-library since stock doesn't have these anymore
    
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: Ic4eb878b63bff4822201f1752164fcccb19abd76

--- device.mk
+++ device.mk
@@ -71,10 +71,6 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
