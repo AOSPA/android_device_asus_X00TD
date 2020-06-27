fca41deba7d2ba61085b4c3b3406e849c889a820
Mr Mesquita <mickaelmendes50@gmail.com>
2020-06-27 11:39:43 +0000

    X00TD: Drop powerhal
    * Following: https://gerrit.aospa.co/c/AOSPA/android_vendor_qcom-opensource_power/+/9795
    and
    https://gerrit.aospa.co/c/AOSPA/android_device_pa_sepolicy/+/11473
    
    Change-Id: I957ae785973e80cf0f0e56c0045606e8efbee4b0
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- device.mk
+++ device.mk
@@ -322,10 +322,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 #PRODUCT_ENFORCE_RRO_TARGETS := \
 #    framework-res
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
