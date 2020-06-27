dbc9c4f61769fcc749655ae9ba82a31a95cb2869
SayanAcharya <rowdysayan99@gmail.com>
2020-06-27 11:58:02 +0000

    X00TD: Drop powerhint.xml
    * We don't use it
    
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: I5eb82ba834fbd15cec49a3e5976c47898f721438

--- device.mk
+++ device.mk
@@ -322,9 +322,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 #PRODUCT_ENFORCE_RRO_TARGETS := \
 #    framework-res
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
