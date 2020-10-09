c6713f942198dff25712717a5743a3c4021963d7
SayanAcharya <rowdysayan99@gmail.com>
2020-10-09 09:30:41 +0000

    X00TD: Build updated nfc packages
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: Ia6dfe8a05944eed9b9ee95ca2810b56af0775b2d

--- device.mk
+++ device.mk
@@ -295,11 +295,22 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
-    NfcNci \
+    com.android.nfc.nq \
+    com.nxp.nfc.nq \
+    com.nxp.nfc.nq.xml \
+    NQNfcNci \
     SecureElement \
     Tag \
+    nfc_nci.nqx.default.hw \
+    vendor.nxp.hardware.nfc@2.0-service \
+    vendor.nxp.nxpnfclegacy@1.0.vendor \
+    libnqnfc-nci \
+    libnqnfc-nci_jni \
+    libsn100nfc_nci_jni \
+    libsn100nfc_nci \
+    se_nq_extn_client
 
 # OMX
 PRODUCT_PACKAGES += \
