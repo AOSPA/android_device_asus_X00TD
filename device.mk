4ea90959fb685a1640641128c594ccbc04a12009
SayanAcharya <rowdysayan99@gmail.com>
2020-11-27 15:50:34 +0000

    X00TD: Use vibrator component from qcom/common
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: Ia75a59efdced8b713fc96f1cba2b422f7151a52c

--- device.mk
+++ device.mk
@@ -143,7 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-  
+
 PRODUCT_PACKAGES += \
     copybit.sdm660 \
     gralloc.sdm660 \
@@ -410,6 +410,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     telephony \
     usb \
+    vibrator \
     wfd-legacy
 
 # Sensors
