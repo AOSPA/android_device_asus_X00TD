4c150145da9a0c8992d24163a905385878acec7a
SayanAcharya <rowdysayan99@gmail.com>
2020-12-04 04:52:35 +0000

    X00TD: Build secure_element v1.2
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: I32f4dd54b539a7e31186a97b3f0f00c1d660f5c5

--- device.mk
+++ device.mk
@@ -368,7 +368,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.2 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.2 \
     rild \
     librmnetctl \
     libxml2 \
