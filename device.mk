fde1b439ee4285e865d0fe619c7d60cab2cb621a
SayanAcharya <rowdysayan99@gmail.com>
2021-03-11 20:37:11 +0000

    X00TD: Move kernel version definition to device.mk
    Change-Id: Ie223e13892ccf7dda18487cb728de2d1a21fed3d

--- device.mk
+++ device.mk
@@ -230,6 +230,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+TARGET_KERNEL_VERSION := 4.19
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
