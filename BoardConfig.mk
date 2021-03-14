7cef17da6a9c61b3306ceeee6863478568379d58
SayanAcharya <rowdysayan99@gmail.com>
2021-03-14 13:28:44 +0000

    X00TD: Move compatibility matrix to qcom/common
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: I6365af3c13ee4492e8ea2d3fba381182c0e0249f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,8 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/device_framework_compatibility_matrix.xml \
