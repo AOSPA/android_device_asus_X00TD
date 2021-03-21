a4a8e77938ae197fb2b8b790d3ed391fb15a68ab
SayanAcharya <rowdysayan99@gmail.com>
2021-03-21 18:39:20 +0000

    X00TD: Override compress voip prop and flag
    * As caf set them as false for sdm660 but we need them
    * No need to override pcm voip prop now
    
    Change-Id: I0a5baf70c55a11f5429ba45e079a973193885326

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -160,6 +160,6 @@ void vendor_load_properties()
     property_override("persist.vendor.audio.fluence.voicerec", "true");
     property_override("persist.vendor.audio.fluence.speaker", "false");
     property_override("ro.vendor.audio.sdk.fluencetype", "fluence");
-    property_override("vendor.voice.path.for.pcm.voip", "false");
+    property_override("vendor.audio.feature.compr_voip.enable", "true");
     dalvik_properties();
 }
