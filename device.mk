29420a5329d4a0e7b9a308a4def2f11d07e574d3
SayanAcharya <rowdysayan99@gmail.com>
2021-04-03 18:43:18 +0000

    X00TD: Move audio packages to qcom/common
    Change-Id: I34720a2ffbddd3871f22958739a14307d62187f4

--- device.mk
+++ device.mk
@@ -54,7 +54,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
-    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
@@ -67,25 +66,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
-    audio.a2dp.default \
     audio_amplifier.sdm660 \
-    audio.primary.sdm660 \
-    audio.r_submix.default \
-    audio.usb.default \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    liba2dpoffload \
-    libaudio-resampler \
-    libcirrusspkrprot \
-    libcomprcapture \
-    libexthwplugin \
-    libhdmiedid \
-    libhfp \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libsndmonitor \
-    libspkrprot \
-    libvolumelistener \
     tinymix \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor
 
@@ -316,12 +298,7 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libc2dcolorconvert \
     libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
